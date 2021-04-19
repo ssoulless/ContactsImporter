c348f814b3d0d082bfc6bda91e9a250f988c86fb
Sebastian Velandia <sebas.velandia27@gmail.com>
2021-04-18 22:10:01 -0500

    Sanitized the headers returned by papaparse

--- app/javascript/packs/contacts.js
+++ app/javascript/packs/contacts.js
@@ -1,16 +1,11 @@
 jQuery(() => {
   $(".upload-contacts-button").on("click", (e) => {
-    console.log("Button clicked");
-
-    if (
-      typeof document.getElementById("contacts_file_field").files.length <= 0
-    ) {
+    if (document.getElementById("contacts_file_field").files.length <= 0) {
       alert("Please select a file");
       return;
     }
 
     const file = document.getElementById("contacts_file_field").files[0];
-
     let fileHeaders;
 
     papa.parse(file, {
@@ -19,7 +14,8 @@ jQuery(() => {
         console.log(err, reason);
       },
       complete: function (results) {
-        fileHeaders = results.meta["fields"];
+        //Sanitize the headers applying trim to remove white spaces
+        fileHeaders = results.meta["fields"].map((header) => header.trim());
         console.log("Finished:", fileHeaders);
       },
     });
